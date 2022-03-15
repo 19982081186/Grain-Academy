6b6a92517c274e33804c57dc20238c98cc74cfbf
19982081186 <3020374226@qq.com>
2022-03-15 16:19:02 +0800

    统计分析微服务模块

---
+++ service/service_statistics/src/main/java/com/atguigu/staservice/mapper/StatisticsDailyMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.staservice.mapper;
+
+import com.atguigu.staservice.entity.StatisticsDaily;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 网站统计日数据 Mapper 接口
+ * </p>
+ *
+ * @author fushengroumeng
+ * @since 2022-03-07
+ */
+public interface StatisticsDailyMapper extends BaseMapper<StatisticsDaily> {
+
+}
